4ad98f719a193890f8ba161b384cd3a496941ad7
borzoni <borzoni@mail.ru>
2013-04-06 03:49:14 +0400

    Basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AviaParser::Application.config.secret_token = '8d6a2d6c4f72438c01d866d29bbea8e9e037c33a24bbdb0410ca1d8cb41266c0b52f7efaa2c3913da7002bead4815b0136dfa085c5e730e0122bf186573cc4f3'
