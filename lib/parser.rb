8a484513af4587ebad5435dd9b8243d0d59ededc
borzoni <borzoni@mail.ru>
2013-04-06 10:21:16 +0400

    Auxiliary parser functions done

---
+++ lib/parser.rb
@@ -0,0 +1,38 @@
+class Parser
+  class << self
+    def parse(from_date, to_date)
+      start, dif = process_interval(from_date, to_date)  
+      url = make_full_url(start, dif)
+      response = get_response(url)
+    end
+
+    private
+    
+    # conversion subroutine
+    # also simple validation
+    def process_interval(date_from, date_to)
+      from = Date.parse(date_from)
+      to =  Date.parse(date_to)
+      # adjust dates
+      from = Date.today if from < Date.today
+      to = Date.today if to < Date.today
+      if from > to
+        raise ArgumentError.new('Date of Arrival is less than Departure Date')
+      end
+      [from, (to - from).to_i]
+    end
+    
+    def make_full_url(start_endpoint, dif)
+      main_part = "http://online.nectravel.ru/freight_monitor/"
+      query_string = {
+          :samo_action => 'FREIGHTS',                       # requested action. dunno what 'samo' stands for.
+          :TOWNFROMINC => '2',                              # town?? towns rarely have airports). Anyway it's the settlement of departure
+          :STATEINC => '3',                                 # the desired country 
+          :TOWNTOINC => '5',                                # the desired settlement
+          :CHECKIN => start_endpoint.strftime('%Y%m%d'),    # from endpoint
+          :NIGHTS_FROM => dif,                        # let it be the duration
+      }
+      main_part + "?#{query_string.to_param}"
+    end
+  end
+end
