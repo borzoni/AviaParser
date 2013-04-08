6c57d4afdd8371781d300836729ec1cde561b2f0
borzoni <borzoni@mail.ru>
2013-04-08 12:47:24 +0400

    labirint controller cleanup.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from Exception, :with => :error_render_method
+
+  def error_render_method
+    respond_to do |type|
+      type.html { render :status => 404 }
+      type.all  { render :status => 404 }
+    end
+    true
+  end
 end
--- app/controllers/labirint_controller.rb
+++ app/controllers/labirint_controller.rb
@@ -2,10 +2,6 @@ class LabirintController < ApplicationController
   layout false, except: [:index]
 
   def parse
-    begin
-      @flights = Parser.parse(params[:date_from], params[:date_to])
-    rescue
-      render:status => 500
-    end
+    @flights = Parser.parse(params[:date_from], params[:date_to])
   end
 end
